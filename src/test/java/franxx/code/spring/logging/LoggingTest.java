a8c7b341f5567f3f2517d017ddf4a2b4b5dd9b5f
Hilmi AM <imiia75775@proton.me>
2024-06-25 16:09:55 +0700

    logging level

--- src/test/java/franxx/code/spring/logging/LoggingTest.java
+++ src/test/java/franxx/code/spring/logging/LoggingTest.java
@@ -3,9 +3,11 @@ package franxx.code.spring.logging;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @Slf4j
+@TestPropertySource("classpath:application-test.properties")
 public class LoggingTest {
 
     @Test
